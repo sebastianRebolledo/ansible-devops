ba5b1765ad773666a58b467ba675b2d5c955dc2e
Sebastian <issareme@hotmail.com>
2021-05-31 23:31:46 -0500

    upload file installdocker.sh

---
+++ installdocker.sh
@@ -0,0 +1,11 @@
+echo 'Install Docker'
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker ubuntu
+sudo usermod -aG docker jenkins
+sudo service jenkins restart
